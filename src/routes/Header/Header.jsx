ea265fb75a4883fd32cdd615a7986078677e5bcb
Helvin Rymer <helvinrymer@helvins-mbp-2.mynetworksettings.com>
2022-11-02 14:01:59 -0400

    styles and makes changes to header

--- src/routes/Header/Header.jsx
+++ src/routes/Header/Header.jsx
@@ -3,23 +3,31 @@ import { Outlet, Link } from 'react-router-dom'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-const Header = ({ props, name }) => {
+const Header = ({ props }) => {
 	return (
 		<Fragment>
-			<header>
-				<Row>
-					<Col sm={2} className='text-center'>
+			<header className='nav__bar'>
+				<Row className='mx-auto my-0 py-3'>
+					<Col sm={6} className='text-center'>
 						<div>
-							<p className='didot'>{name}</p>
+							<p className='didot m-0'>
+								{props.nav_links.map(
+									(e, i) =>
+										e.url === '/' && <Link to='/'>{props.nick_name}</Link>
+								)}
+							</p>
 						</div>
 					</Col>
-					<Col sm={10} className='text-right'>
+					<Col sm={6} className='text-center'>
 						<ul>
-							{props.map((e, i) => (
-								<li key={i} className='px-2'>
-									<Link to={e.url}>{e.link_text}</Link>
-								</li>
-							))}
+							{props.nav_links.map(
+								(e, i) =>
+									e.url !== '/' && (
+										<li key={i} className='px-3'>
+											<Link to={e.url}>{e.link_text}</Link>
+										</li>
+									)
+							)}
 						</ul>
 					</Col>
 				</Row>
