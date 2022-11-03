b8e8d2d3ae995eaec4eef27159a0c66752b0ea06
Helvin Rymer <helvinrymer@helvins-mbp-2.mynetworksettings.com>
2022-11-03 19:21:16 -0400

    scaffold about page/resume

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-const About = () => {
-	return <div>About</div>
+const About = ({ props }) => {
+	return (
+		<Container className='egg-shell-font' fluid>
+			<Container className='text-center py-4 mx-auto'>
+				<h1 className='display-1'>Helvin Rymer</h1>
+			</Container>
+			<Row className='mx-auto max-w-1024 jobs'>
+				<Col className='jobs__cards' sm={6}>
+					<div className='jobs__cards__card p-3'>
+						<h3>iMedia Inc</h3>
+						<p className='m-0'>Position: Front End Developer</p>
+						<p>Time:</p>
+					</div>
+					<div className='jobs__cards__card p-3'>
+						<h3>MrRymer.com</h3>
+						<p className='m-0'>Position: Full Stack Developer</p>
+						<p>Time:</p>
+					</div>
+					<div className='jobs__cards__card p-3'>
+						<h3>Trailways.com</h3>
+						<p className='m-0'>Position: Front End Developer</p>
+						<p>Time:</p>
+					</div>
+				</Col>
+				<Col sm={6}>
+					<div className='jobs__cards__card'>
+						<h3>stuff</h3>
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	)
 }
 
 export default About
+
