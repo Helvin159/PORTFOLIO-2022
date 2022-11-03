c9b70b3bb1f4e870dc53b07bd37672fe813b3edd
Helvin Rymer <helvinrymer@helvins-mbp-2.mynetworksettings.com>
2022-11-03 16:48:26 -0400

    updates cards to sidebyside cards, body background, main max-width, adds ADP.com to data, adds isOdd function to lib folder

--- src/components/ProjectsShowcase/index.js
+++ src/components/ProjectsShowcase/index.js
@@ -1,33 +1,40 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import isOdd from '../../lib/isOdd'
 
 const ProjectsShowcase = ({ props }) => {
 	// console.log(props)
+
 	return (
-		<Row className='projects mx-auto'>
+		<div className='projects mx-auto'>
 			{props.projects.map((i, k) => (
-				<Fragment>
-					<style>{`
-						.card-${k}{
-							background: linear-gradient( 0deg, rgba(0,0,0,0.9), rgba(0,0,0,.9)), url(${i.screenshot});
-							background-size:cover;
-							background-position:center center;
-							background-repeat:no-repeat;
-						}
-					`}</style>
-					<Col sm={6} key={k} className={`projects__cards card-${k}`}>
+				<Row key={k} className={`${isOdd(k) && 'flex-row-reverse'}`}>
+					<Col
+						sm={6}
+						key={`${k}-${i.projectName}`}
+						className={`projects__cards card-${k}`}>
 						<div className='projects__cards__card'>
-							<h3>{i.projectName}</h3>
-							{i.workDone !== '' && <p>{i.workDone}</p>}
-							{console.log(i)}
+							<img
+								className='img-responsive'
+								alt={i.projectName}
+								src={i.screenshot}
+							/>
+						</div>
+					</Col>
+					<Col sm={6} key={`${k}`} className={`projects__cards`}>
+						<div className='projects__cards__card'>
+							<div className='text-center'>
+								<h3>{i.projectName}</h3>
+								{i.workDone !== '' && <p>{i.workDone}</p>}
+							</div>
 
 							<ul>
 								{i.languages.map((i, k) => (
 									<li key={k}>{i.lang}</li>
 								))}
 							</ul>
-							{console.log(i)}
+
 							<div className='py-3'>
 								<a href={i.url} target='_blank' rel='noreferrer'>
 									Visit site
@@ -35,9 +42,9 @@ const ProjectsShowcase = ({ props }) => {
 							</div>
 						</div>
 					</Col>
-				</Fragment>
+				</Row>
 			))}
-		</Row>
+		</div>
 	)
 }
 
