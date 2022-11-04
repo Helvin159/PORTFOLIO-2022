ed421e2766b27834c7239d8248c60ca6315996de
Helvin Rymer <helvinrymer@helvins-mbp-2.mynetworksettings.com>
2022-11-04 07:56:33 -0400

    scaffolds about page

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -4,32 +4,31 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 const About = ({ props }) => {
+	// console.log(props, 'footer')
 	return (
 		<Container className='egg-shell-font' fluid>
 			<Container className='text-center py-4 mx-auto'>
-				<h1 className='display-1'>Helvin Rymer</h1>
+				<h1 className='display-1'>{props.name}</h1>
 			</Container>
 			<Row className='mx-auto max-w-1024 jobs'>
 				<Col className='jobs__cards' sm={6}>
-					<div className='jobs__cards__card p-3'>
-						<h3>iMedia Inc</h3>
-						<p className='m-0'>Position: Front End Developer</p>
-						<p>Time:</p>
-					</div>
-					<div className='jobs__cards__card p-3'>
-						<h3>MrRymer.com</h3>
-						<p className='m-0'>Position: Full Stack Developer</p>
-						<p>Time:</p>
-					</div>
-					<div className='jobs__cards__card p-3'>
-						<h3>Trailways.com</h3>
-						<p className='m-0'>Position: Front End Developer</p>
-						<p>Time:</p>
-					</div>
+					{props.jobs.map((i, k) => (
+						<div className='jobs__cards__card p-3' key={k}>
+							<h3>{i.company}</h3>
+							<p className='m-0'>Position: {i.position}</p>
+							<p>Time: {i.total_time}</p>
+						</div>
+					))}
 				</Col>
 				<Col sm={6}>
 					<div className='jobs__cards__card'>
-						<h3>stuff</h3>
+						<h3>Skills</h3>
+					</div>
+					<div className='jobs__cards__card'>
+						<h3>Education</h3>
+					</div>
+					<div className='jobs__cards__card'>
+						<h3>Continuing Education</h3>
 					</div>
 				</Col>
 			</Row>
