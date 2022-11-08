9abfc3a29646388e1db94b00353b688611241e36
Helvin Rymer <helvinrymer@helvins-mbp-2.mynetworksettings.com>
2022-11-07 21:47:52 -0500

    adds new route

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import axios from 'axios'
+
+const ContactForm = () => {
+	const url = window.location.origin
+
+	const successMsg = () => {
+		alert('Sent successfully')
+	}
+
+	const errorMsg = () => {
+		alert('Message failed')
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+
+		// console.log(e)
+
+		let payload = { name: 'helvin' }
+
+		axios
+			.post(`${url}`, JSON.stringify(payload))
+			.then((res) => {
+				console.log(res, 'response')
+				console.log(JSON.stringify(payload))
+				successMsg()
+			})
+			.catch((er) => {
+				console.log(er, 'error')
+				console.log(JSON.stringify(payload))
+				errorMsg()
+			})
+	}
+	return (
+		<Container className='mx-auto max-w-860 text-center contact__form__container'>
+			<form
+				method='post'
+				name='contact_form'
+				encType='multipart/form-data'
+				className='contact__form__container__form'>
+				<input
+					type='text'
+					name='name_info'
+					id='nameInfo'
+					placeholder='Name'
+					required
+				/>
+				<input
+					type='email'
+					name='email_info'
+					id='emailInfo'
+					placeholder='Email Address'
+					required
+				/>
+				<input
+					type='submit'
+					name='submit_info'
+					id='submitInfo'
+					value='Send'
+					onClick={onSubmit}
+				/>
+			</form>
+		</Container>
+	)
+}
+
+export default ContactForm
+
--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -1,72 +1,14 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import axios from 'axios'
+import ContactForm from '../../components/ContactForm'
 
 const Contact = () => {
-	const url = window.location.origin
-
-	const successMsg = () => {
-		alert('Sent successfully')
-	}
-
-	const errorMsg = () => {
-		alert('Message failed')
-	}
-
-	const onSubmit = (e) => {
-		e.preventDefault()
-
-		// console.log(e)
-
-		let payload = { name: 'helvin' }
-
-		axios
-			.post(`${url}`, JSON.stringify(payload))
-			.then((res) => {
-				console.log(res, 'response')
-				console.log(JSON.stringify(payload))
-				successMsg()
-			})
-			.catch((er) => {
-				console.log(er, 'error')
-				console.log(JSON.stringify(payload))
-				errorMsg()
-			})
-	}
 	return (
 		<Container fluid className='contact'>
 			<Container className='mx-auto text-center contact__title'>
-				<h1 className='display-1'>Let's chat!</h1>
+				<h1 className='display-1 egg-shell-font'>Let's chat!</h1>
 			</Container>
-			<Container className='mx-auto max-w-860 text-center contact__form__container'>
-				<form
-					method='post'
-					name='contact_form'
-					encType='multipart/form-data'
-					className='contact__form__container__form'>
-					<input
-						type='text'
-						name='name_info'
-						id='nameInfo'
-						placeholder='Name'
-						required
-					/>
-					<input
-						type='email'
-						name='email_info'
-						id='emailInfo'
-						placeholder='Email Address'
-						required
-					/>
-					<input
-						type='submit'
-						name='submit_info'
-						id='submitInfo'
-						value='Send'
-						onClick={onSubmit}
-					/>
-				</form>
-			</Container>
+			<ContactForm />
 		</Container>
 	)
 }
